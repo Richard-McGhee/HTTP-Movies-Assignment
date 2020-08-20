d2a05edbcc51d9b8113e79338dcf876898abd2be
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-19 21:56:55 -0400

    got form working to render with movie values from parameter, build handlechanges function and built placeholder handlesubmit function that pushes user to paramter value movie. Just need to work on the put request next.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory()
 
   const fetchMovie = (id) => {
     axios
@@ -17,6 +18,11 @@ function Movie({ addToSavedList }) {
   const saveMovie = () => {
     addToSavedList(movie);
   };
+
+  const updateMovie = (evt) => {
+    evt.preventDefault()
+    push(`/update-movie/${params.id}`)
+  }
 
   useEffect(() => {
     fetchMovie(params.id);
@@ -33,6 +39,7 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button onClick={updateMovie}>Update Movie</button>
     </div>
   );
 }
---
+++ client/src/Movies/MovieUpdate.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useParams, useHistory } from 'react-router-dom'
+import axios from 'axios'
+
+const MovieUpdate = (props) => {
+    const { movieList, setMovieList } = props
+    const params = useParams()
+    const { push } = useHistory()
+    const initState = {
+        id: "",
+        title: "",
+        director: "",
+        metascore: 0,
+        stars: []
+    }
+    const [newInfo, setNewInfo] = useState(initState)
+
+    useEffect(() => {
+        axios
+      .get(`http://localhost:5000/api/movies/${params.id}`)
+      .then(res => {
+          console.log(res.data)
+          setNewInfo(res.data)
+          console.log(newInfo)
+      })
+      .catch(err => console.log(err.response));
+    },[params.id])
+
+    const handleChanges = evt => {
+        setNewInfo({
+            ...newInfo,
+            [evt.target.name]: evt.target.value
+        })
+    }
+    const handleSubmit = evt => {
+        evt.preventDefault()
+
+        push(`/movies/${params.id}`)
+    }
+
+    if(!newInfo) {
+        return <h2>...LOADING INFO</h2>
+    }
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <input type="text"
+            name="title"
+            value={newInfo.title}
+            onChange={handleChanges}/>
+            <input type="text"
+            name="director"
+            value={newInfo.director}
+            onChange={handleChanges}/>
+            <input type="number"
+            name="metascore"
+            value={newInfo.metascore}
+            onChange={handleChanges}/>
+            <button>Update Movie</button>
+        </form>
+     );
+}
+ 
+export default MovieUpdate;
